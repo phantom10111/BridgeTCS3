3bf6a29538125c38e759fc0fcd70ab83ad2e56d7
phantom10111 <phantom10111@gmail.com>
2014-03-04 17:33:21 +0100

    Add Denomination and tests

---
+++ src/Denomination.hpp
@@ -0,0 +1,13 @@
+#ifndef _DENOMINATION_H_
+#define _DENOMINATION_H_
+
+enum class Denomination
+{
+	CLUBS,
+	DIAMONDS,
+	HEARTS,
+	SPADES,
+	VOID
+};
+
+#endif
---
+++ test/DenominationTest.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include "Denomination.hpp"
+
+TEST(Denomination, Order)
+{
+	ASSERT_TRUE(Denomination::CLUBS < Denomination::DIAMONDS);
+	ASSERT_TRUE(Denomination::CLUBS < Denomination::HEARTS);
+	ASSERT_TRUE(Denomination::CLUBS < Denomination::SPADES);
+	ASSERT_TRUE(Denomination::CLUBS < Denomination::VOID);
+	ASSERT_TRUE(Denomination::DIAMONDS < Denomination::HEARTS);
+	ASSERT_TRUE(Denomination::DIAMONDS < Denomination::SPADES);
+	ASSERT_TRUE(Denomination::DIAMONDS < Denomination::VOID);
+	ASSERT_TRUE(Denomination::HEARTS < Denomination::SPADES);
+	ASSERT_TRUE(Denomination::HEARTS < Denomination::VOID);
+	ASSERT_TRUE(Denomination::SPADES < Denomination::VOID);
+}
