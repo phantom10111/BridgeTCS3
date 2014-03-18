057b876c393e03b1c9f7ac30e7417b53cd82be57
phantom10111 <phantom10111@gmail.com>
2014-03-18 17:27:32 +0100

    Level to int

--- src/Call.cpp
+++ src/Call.cpp
@@ -1,7 +1,8 @@
 #include "Call.hpp"
 #include "CallType.hpp"
 #include "Denomination.hpp"
-#include "Level.hpp"
+
+#include <stdexcept>
 
 Call Call::pass()
 {
@@ -18,7 +19,9 @@ Call Call::redbl()
 	return Call(CallType::REDOUBLE);
 }
 
-Call Call::bid(Level level, Denomination denomination)
+Call Call::bid(int level, Denomination denomination)
 {
+	if(level < 1 || level > 7)
+		throw std::invalid_argument("Incorrect level");
 	return Call(CallType::BID, level, denomination);
 }
