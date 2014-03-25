577805bc9939e35711561918d59ca8e2618542d3
edoipi <implicent@gmail.com>
2014-03-25 17:43:37 +0100

    BridgeDeck constructor and shuffle

--- src/BridgeDeck.cpp
+++ src/BridgeDeck.cpp
@@ -1,13 +1,26 @@
 #include "BridgeDeck.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
 
-BridgeDeck::BridgeDeck(){
-	
-	
+BridgeDeck::BridgeDeck()
+{
+	Suit suits[]={Suit::SPADES, Suit::HEARTS, Suit::DIAMONDS, Suit::CLUBS};
+	Rank ranks[]={Rank::TWO, Rank::THREE, Rank::FOUR, Rank::FIVE, Rank::SIX, Rank::SEVEN, Rank::EIGHT, Rank::NINE, Rank::TEN, Rank::JACK, Rank::QUEEN, Rank::KING, Rank::ACE};
+	for(Suit s: suits)
+	{
+		for(Rank r: ranks)
+		{
+			cards.push_back(new Card(s, r));
+		}
+	}
 }
-void BridgeDeck::shuffle(){
-	
-	
+void BridgeDeck::shuffle()
+{
+	std::srand ( unsigned ( std::time(0) ) );
+	std::random_shuffle(cards.begin(), cards.end());
 }
-Card& BridgeDeck::getCard(){
-	
+Card& BridgeDeck::getCard()
+{
+	//return cards[0];
 }
