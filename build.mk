bcfa23d1a9a91e2f1afa7318e2f3af79d06e2906
phantom10111 <phantom10111@gmail.com>
2014-04-08 12:34:22 +0200

    Poprawiona kompilacja testów

--- build.mk
+++ build.mk
@@ -2,7 +2,6 @@ override CLEAN = $(RM) *.o .*.d
 override COMPILE_FLAGS = -std=c++11 -MMD -MP -I$(ROOT)/src
 override LINK = $(LINK.cpp) $^ $(LOADLIBES) $(LDLIBS) $(LINK_FLAGS) $(OUTPUT_OPTION)
 override LINK_FLAGS :=
-override OBJECT_FILES = $(patsubst %.cpp,%.o,$(wildcard *.cpp))
 
 CXXFLAGS := -O2
 
